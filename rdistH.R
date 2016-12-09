cc496c7e3a29f457e462b0cc1fb21c29f0fd7bf0
eliardocosta <eliardocosta@gmail.com>
2016-12-09 00:33:23 -0200

    Initial commit

---
+++ rdistH.R
@@ -0,0 +1,31 @@
+rdistH <- function(nsam, x, w, lam0, phi, alpha, nburn = 1E2) {
+  n <- length(x)
+  lam <- rgamma(n, shape = phi, rate = phi/lam0)
+  for (r in 1:nburn) { # COMECO BURN-IN
+    for (i in 1:n) {
+      q0 <- alpha*dnbinom(x[i], mu = w*lam0, size = phi)
+      qk <- dpois(x[i], lambda = w*lam[-i])
+      cn <- q0 + sum(qk) # constante de normalização
+      q0n <- q0/cn
+      qkn <- qk/cn
+      u <- runif(1)
+      lam[i] <- ifelse(u <= q0n, rgamma(1, shape = phi + x[i], rate = (w + phi/lam0)),
+                       sample(lam[-i], 1, prob = qkn))
+    }
+  } # FIM BURN-IN
+  sam.lam <- matrix(NA, nsam, n)
+  for (r in 1:nsam) { # COMECO AMOSTRAGEM
+    for (i in 1:n) {
+      q0 <- alpha*dnbinom(x[i], mu = w*lam0, size = phi)
+      qk <- dpois(x[i], lambda = w*lam[-i])
+      cn <- q0 + sum(qk) # constante de normalização
+      q0n <- q0/cn
+      qkn <- qk/cn
+      u <- runif(1)
+      lam[i] <- ifelse(u <= q0n, rgamma(1, shape = phi + x[i], rate = (w + phi/lam0)),
+                       sample(lam[-i], 1, prob = qkn))
+    }
+    sam.lam[r, ] <- lam
+  } # FIM AMOSTRAGEM
+  return(sam.lam)
+}
---
+++ ssPoiDP2.R
@@ -0,0 +1,131 @@
+ssPoiDP2 <- function(lam0, phi, w, rho, alpha, len.max, cgrid, inc1 = 1E2 , inc2 = 5E1, inc3 = 1E1, R = 1E2, n0 = 3) {
+  cat("Call for PoiDP \n")
+  cat("phi =", phi," ;", "w =", w, " ;", "alpha =", alpha ,"len.max =", len.max, "\n")
+  n <- n0
+  len.med <- len.max+1
+  while (len.med > len.max) {
+    n <- n+inc1
+    len <- numeric()
+    for (i in 1:R) {
+      x <- rnbinom(n, mu = w*lam0, size = phi)
+      obj.ppost <- exp.postDPmix(x = x, w = w, lam0 = lam0, phi = phi, alpha = alpha, cgrid = cgrid, nsam = 5E1)
+      obj.dpost <- extract.prob(obj.ppost)
+  #    plot(obj.dpost$lam, obj.dpost$ddist, type = "h")
+      vals <- obj.dpost$lam
+      probs <- obj.dpost$ddist
+      conj.vals <- numeric()
+      cob <- 0
+      while (sum(cob) < 1-rho) {
+        conj.ind <- which.max(probs) 
+        conj.vals <- append(conj.vals, vals[conj.ind])
+        for (i in 1:length(conj.vals)) {
+          cob[i] <- obj.dpost$ddist[obj.dpost$lam == conj.vals[i]]
+        }
+        vals <- vals[-conj.ind]
+        probs <- probs[-conj.ind]
+      }
+      len <- append(len, length(conj.vals)*obj.ppost$cgrid)
+      cat(mean(len), " ")
+    }
+    cat("len.med e n \n")
+    len.med <- mean(len)#; 
+    cat(len.med, n, "\n")
+  }
+  # SEGUNDO LOOP COM INCREMENTO IGUAL A inc2
+  n <- n-inc1
+  len.med <- len.max+1
+  while (len.med > len.max) {
+    n <- n+inc2
+    len <- numeric()
+    for (i in 1:R) {
+      x <- rnbinom(n, mu = w*lam0, size = phi)
+      obj.ppost <- exp.postDPmix(x = x, w = w, lam0 = lam0, phi = phi, alpha = alpha, cgrid = cgrid, nsam = 5E1)
+      obj.dpost <- extract.prob(obj.ppost)
+   #   plot(obj.dpost$lam, obj.dpost$ddist, type = "h")
+      vals <- obj.dpost$lam
+      probs <- obj.dpost$ddist
+      conj.vals <- numeric()
+      cob <- 0
+      while (sum(cob) < 1-rho) {
+        conj.ind <- which.max(probs) 
+        conj.vals <- append(conj.vals, vals[conj.ind])
+        for (i in 1:length(conj.vals)) {
+          cob[i] <- obj.dpost$ddist[obj.dpost$lam == conj.vals[i]]
+        }
+        vals <- vals[-conj.ind]
+        probs <- probs[-conj.ind]
+      }
+      len <- append(len, length(conj.vals)*obj.ppost$cgrid)
+      cat(mean(len), " ")
+    }
+    cat("len.med e n \n")
+    len.med <- mean(len)#; 
+    cat(len.med, n, "\n")
+  }
+  # TERCEIRO LOOP COM INCREMENTO IGUAL A inc3
+  n <- n-inc2
+  len.med <- len.max+1
+  while (len.med > len.max) {
+    n <- n+inc3
+    len <- numeric()
+    for (i in 1:R) {
+      x <- rnbinom(n, mu = w*lam0, size = phi)
+      obj.ppost <- exp.postDPmix(x = x, w = w, lam0 = lam0, phi = phi, alpha = alpha, cgrid = cgrid, nsam = 5E1)
+      obj.dpost <- extract.prob(obj.ppost)
+  #    plot(obj.dpost$lam, obj.dpost$ddist, type = "h")
+      vals <- obj.dpost$lam
+      probs <- obj.dpost$ddist
+      conj.vals <- numeric()
+      cob <- 0
+      while (sum(cob) < 1-rho) {
+        conj.ind <- which.max(probs) 
+        conj.vals <- append(conj.vals, vals[conj.ind])
+        for (i in 1:length(conj.vals)) {
+          cob[i] <- obj.dpost$ddist[obj.dpost$lam == conj.vals[i]]
+        }
+        vals <- vals[-conj.ind]
+        probs <- probs[-conj.ind]
+      }
+      len <- append(len, length(conj.vals)*obj.ppost$cgrid)
+      cat(mean(len), " ")
+    }
+    cat("len.med e n \n")
+    len.med <- mean(len)#; 
+    cat(len.med, n, "\n")
+  }
+  # QUARTO LOOP COM INCREMENTO IGUAL A 1
+  n <- n-inc3
+  len.med <- len.max+1
+  while (len.med > len.max) {
+    n <- n+1
+    #print(n)
+    len <- numeric()
+    for (i in 1:R) {
+      x <- rnbinom(n, mu = w*lam0, size = phi)
+      obj.ppost <- exp.postDPmix(x = x, w = w, lam0 = lam0, phi = phi, alpha = alpha, cgrid = cgrid, nsam = 5E1)
+      obj.dpost <- extract.prob(obj.ppost)
+   #   plot(obj.dpost$lam, obj.dpost$ddist, type = "h")
+      vals <- obj.dpost$lam
+      probs <- obj.dpost$ddist
+      conj.vals <- numeric()
+      cob <- 0
+      while (sum(cob) < 1-rho) {
+        conj.ind <- which.max(probs) 
+        conj.vals <- append(conj.vals, vals[conj.ind])
+        for (i in 1:length(conj.vals)) {
+          cob[i] <- obj.dpost$ddist[obj.dpost$lam == conj.vals[i]]
+        }
+        vals <- vals[-conj.ind]
+        probs <- probs[-conj.ind]
+      }
+      len <- append(len, length(conj.vals)*obj.ppost$cgrid)
+      cat(mean(len), " ")
+    }
+    cat("len.med e n \n")
+    len.med <- mean(len)#; 
+    cat(len.med, n, "\n")
+  }
+  cat("n (PoiDP) =", n, "Comp est =", len.med, "\n")
+}  # FIM
+  
+# ssPoiDP2(lam0 = lam0, phi = 5, w = w, rho = 0.05, alpha = 1, len.max = 4, inc1 = 1E2, inc2 = 5E1, inc3 = 1E1)
