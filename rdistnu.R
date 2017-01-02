5d8b4d805593e700ff0e8331773164d246b68d2b
eliardocosta <eliardocosta@gmail.com>
2017-01-02 13:52:40 -0200

    initial commit

---
+++ rdistnu.R
@@ -0,0 +1,36 @@
+rdistnu <- function(nsam, x, w, lam0, phi, alpha, nburn = 5E1) {
+  n <- length(x)
+  lam <- rgamma(n, shape = phi, rate = phi/lam0)
+    for (r in 1:nburn) { # COMECO BURN-IN
+      for (i in 1:n) { i=1
+      q0 <- alpha*dnbinom(x[i], mu = w*lam0, size = phi)
+      qk <- dpois(x[i], lambda = w*lam[-i])
+      cn <- q0 + sum(qk) # constante de normalização
+      q0n <- q0/cn
+      qkn <- qk/cn
+      u <- runif(1)
+      lam[i] <- ifelse(u <= q0n, rgamma(1, shape = phi + x[i], rate = (w + phi/lam0)),
+                       sample(x = lam[-i], size = 1, prob = qkn))
+      }
+    } # FIM BURN-IN
+    sam.lam <- matrix(NA, nsam, n)
+    for (r in 1:nsam) { # COMECO AMOSTRAGEM
+      for (i in 1:n) {
+        q0 <- alpha*dnbinom(x[i], mu = w*lam0, size = phi)
+        qk <- dpois(x[i], lambda = w*lam[-i])
+        cn <- q0 + sum(qk) # constante de normalização
+        q0n <- q0/cn
+        qkn <- qk/cn
+        u <- runif(1)
+        lam[i] <- ifelse(u <= q0n, rgamma(1, shape = phi + x[i], rate = (w + phi/lam0)),
+                         sample(x = lam[-i], size = 1, prob = qkn))
+      }
+      sam.lam[r, ] <- lam
+    } # FIM AMOSTRAGEM
+    return(sam.lam)
+}
+
+#alpha <- 1.5
+#phi <- 1.5
+#rdistH(nsam = 5, x, w, lam0, phi, alpha, nburn = 1E2)
+#cgrid <- 0.5
