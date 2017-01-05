be6b3f69347c2355a2841044db9a40910d25ac87
eliardocosta <eliardocosta@gmail.com>
2017-01-05 11:23:09 -0200

    normalized the probs

--- extractprob.R
+++ extractprob.R
@@ -1,11 +1,11 @@
 extract.prob <- function(obj) { # obter a distribuição de probabilidade do 'obj'
-  grid.val <- filter(obj$lam, rep(1/2, 2)) # pegando o ponto médio do intervalo
-  #grid.val <- as.numeric(grid.val[-length(grid.val)])
+  grid.val <- filter(obj$lam, rep(1/2, 2)) # pegando o ponto medio do intervalo
   grid.val <- as.numeric(head(grid.val, -1))
   dprob <- numeric()
   for (i in 2:length(obj$fdist)) {
     dprob[i] <- obj$fdist[i]-obj$fdist[i-1]
   }
   dprob <- dprob[-1]
+  dprob <- dprob/sum(dprob)
   return(list(lam = grid.val, ddist = dprob))
-} # FIM TESTE
+} # FIM 
