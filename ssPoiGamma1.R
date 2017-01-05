63dab2b0a2ae99752a5950402b1f972bb3c3b617
eliardocosta <eliardocosta@gmail.com>
2017-01-05 11:27:20 -0200

    minor fixes

--- ssPoiGamma1.R
+++ ssPoiGamma1.R
@@ -1,44 +1,44 @@
 ssPoiGamma1 <- function(lam0, phi, w, rho, crit, len = NULL, 
-                         len.max = NULL, R = 1E3, srep = 1E2) {
-  cat("Call for PoiGamma \n")
-  cat("phi =", phi," ;", "w =", w, " ;", "len.max =", len.max, "\n")
-  if (crit == "ACC") {
-    cov <- 0
-    n <- 0
-    while (mean(cov) < 1-rho) {
-      #print(mean(cov))
-      n <- n+1
+                         len.max = NULL, R = 1E2, srep = 1E2, n0 = 0) {
+  cat("\nCall for PoiGamma \n")
+  if (crit == "CVM") cat("phi =", phi,"; w =", w, "; alpha =", alpha,"; eps =", eps, "\n")
+  if (crit == "CCM1") cat("phi =", phi,"; w =", w, "; alpha =", alpha,"; l =", len, "\n")
+  if (crit == "CCM2") cat("phi =", phi,"; w =", w, "; alpha =", alpha,"; l.max =", len.max, "\n")
+  if (crit == "CVM") {
+    break
+  }
+  if (crit == "CCM1") { # INICIO CRITERIO CCM1
+    cov <- 0 
+    n <- n0
+    while (mean(cov) < 1 - rho) {
+      n <- n + 1
       cov <- numeric()
       for (i in 1:R) {
         s <- rnbinom(srep, mu = n*w*lam0, size = n*phi)
-        psi <- phi+s
-        kappa <- n*w+phi/lam0
-        a <- len/(exp((kappa*len)/(psi-1))-1)
-        cov <- append(cov, mean(pgamma(a+len, shape = psi, rate = kappa)-pgamma(a, 
+        psi <- phi + s
+        kappa <- n*w + phi/lam0
+        a <- len/(exp((kappa*len)/(psi - 1)) - 1)
+        cov <- append(cov, mean(pgamma(a + len, shape = psi, rate = kappa) - pgamma(a, 
                                                                                 shape = psi, rate = kappa)))
       }
     }
-  }
-  if (crit == "ALC") {
-    len <- len.max+1
-    n <- 0
+    cat("n (PoiGamma) =", n, "Cob est =", mean(cov), "\n")
+  } # FIM CRITERIO CCM1
+  if (crit == "CCM2") { # INICIO CRITERIO CCM2
+    len <- len.max + 1
+    n <- n0
     while (mean(len) > len.max) {
-      #print(mean(len))
-      n <- n+1
+      n <- n + 1
       len <- numeric()
       for (i in 1:R) {
         s <- rnbinom(srep, mu = n*w*lam0, size = n*phi)
-        psi <- phi+s
-        kappa <- n*w+phi/lam0
-        len <- append(len, mean(qgamma(1-rho/2, shape = psi,
-                                       rate = kappa)-qgamma(rho/2, 
+        psi <- phi + s
+        kappa <- n*w + phi/lam0
+        len <- append(len, mean(qgamma(1 - rho/2, shape = psi,
+                                       rate = kappa) - qgamma(rho/2, 
                                                             shape = psi, rate = kappa)))
       }
-      #print(mean(len))
     }
     cat("n (PoiGamma) =", n, "Comp est =", mean(len), "\n")
-    cat("\n")
-  }
-  
-  #return(n)
+  } # FIM CRITERIO CCM2
 } # FIM
