b25891b6f1f623be9b4b26d8500686ad3b4a25a9
eliardocosta <eliardocosta@gmail.com>
2017-02-03 11:30:14 -0200

    excluded an unecessary loop

--- ssPoiGamma1.R
+++ ssPoiGamma1.R
@@ -1,5 +1,5 @@
 ssPoiGamma1 <- function(lam0, phi, w, rho, crit, len = NULL, 
-                         len.max = NULL, R = 1E2, srep = 1E2, n0 = 0) {
+                         len.max = NULL, R = 1E3, srep = 1E2, n0 = 0) {
   cat("\nCall for PoiGamma \n")
   if (crit == "CVM") cat("phi =", phi,"; w =", w, "; eps =", eps, "\n")
   if (crit == "CCM1") cat("phi =", phi,"; w =", w, "; l =", len, "\n")
@@ -14,12 +14,14 @@ ssPoiGamma1 <- function(lam0, phi, w, rho, crit, len = NULL,
       n <- n + 1
       cov <- numeric()
       for (i in 1:R) {
-        s <- rnbinom(srep, mu = n*w*lam0, size = n*phi)
+        s <- rnbinom(1, mu = n*w*lam0, size = n*phi)
         psi <- phi + s
         kappa <- n*w + phi/lam0
         a <- len/(exp((kappa*len)/(psi - 1)) - 1)
-        cov <- append(cov, mean(pgamma(a + len, shape = psi, rate = kappa) - pgamma(a, 
-                                                                                shape = psi, rate = kappa)))
+        cov <- append(cov, pgamma(a + len, shape = psi, rate = kappa) - pgamma(a, 
+                                                                                    shape = psi, rate = kappa))
+        #cov <- append(cov, mean(pgamma(a + len, shape = psi, rate = kappa) - pgamma(a, 
+        #                                                                        shape = psi, rate = kappa)))
       }
     }
     cat("n (PoiGamma) =", n, "Cob est =", mean(cov), "\n")
@@ -31,12 +33,15 @@ ssPoiGamma1 <- function(lam0, phi, w, rho, crit, len = NULL,
       n <- n + 1
       len <- numeric()
       for (i in 1:R) {
-        s <- rnbinom(srep, mu = n*w*lam0, size = n*phi)
+        s <- rnbinom(1, mu = n*w*lam0, size = n*phi)
         psi <- phi + s
         kappa <- n*w + phi/lam0
-        len <- append(len, mean(qgamma(1 - rho/2, shape = psi,
+        #len <- append(len, mean(qgamma(1 - rho/2, shape = psi,
+        #                               rate = kappa) - qgamma(rho/2, 
+        #                                                    shape = psi, rate = kappa)))
+        len <- append(len, qgamma(1 - rho/2, shape = psi,
                                        rate = kappa) - qgamma(rho/2, 
-                                                            shape = psi, rate = kappa)))
+                                                              shape = psi, rate = kappa))
       }
     }
     cat("n (PoiGamma) =", n, "Comp est =", mean(len), "\n")
