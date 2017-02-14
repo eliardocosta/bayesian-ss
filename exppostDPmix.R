453576564278e47dfdcb5bdc104fbfd8312c456d
eliardocosta <eliardocosta@gmail.com>
2017-02-14 17:40:58 -0200

    change phi by theta0

--- exppostDPmix.R
+++ exppostDPmix.R
@@ -1,12 +1,12 @@
-exp.postDPmix <- function(x = x, w = w, lam0 = lam0, phi = phi, alpha = alpha, cgrid = 1E-2, nsam = 1E2) {
+exp.postDPmix <- function(x = x, w = w, lam0 = lam0, theta0 = theta0, alpha = alpha, cgrid = 1E-2, nsam = 1E2) {
   n <- length(x)
-  samcon.lam <- rdistnu(nsam = nsam, x = x, w = w, lam0 = lam0, phi = phi, alpha = alpha)
+  samcon.lam <- rdistnu(nsam = nsam, x = x, w = w, lam0 = lam0, theta0 = theta0, alpha = alpha)
   grid <- seq(0, ceiling(max(samcon.lam)), cgrid)
   probs <- matrix(NA, nsam, length(grid))
   probs[ ,1] <- rep(0, nsam)
   for (i in 1:nsam) {
     for (j in 2:length(grid)) {
-      probs[i, j] <- (alpha/(alpha + n))*pgamma(grid[j], shape = phi, rate = phi/lam0)+
+      probs[i, j] <- (alpha/(alpha + n))*pgamma(grid[j], shape = theta0, rate = theta0/lam0)+
         (1/(alpha + n))*(length(which(samcon.lam[i, ] <= grid[j])))
     }
   }
