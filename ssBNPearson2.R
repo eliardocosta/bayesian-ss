6987e87c07bee470d0ec376e889a70c21a7d1e81
eliardocosta <eliardocosta@gmail.com>
2017-03-01 14:24:45 -0300

    added y label and more than a estimate for each n

--- ssBNPearson2.R
+++ ssBNPearson2.R
@@ -9,9 +9,10 @@ ssBNPearson2 <- function(lf, lam0, theta0, phi, w, c, rho = NULL,
   cat("Loss function =", lf, "\n")
   if (lf == 1) cat("lam0 =", lam0, "; theta0 =", theta0, "; phi =", phi,"; w =", w, "; rho =", rho, "; c =", c, "\n")
   if (lf == 2) cat("lam0 =", lam0, "; theta0 =", theta0, "; phi =", phi,"; w =", w, "; gam =", gam, "; c =", c, "\n")
+  ns <- seq(1, nmax, by = 5)
   risk <- numeric()
   if (lf == 1) {
-    for (n in 1:nmax) {
+    for (n in ns) {
       for (i in 1:nrep) {
         loss <- numeric()
         sn <- numeric()
@@ -30,7 +31,7 @@ ssBNPearson2 <- function(lf, lam0, theta0, phi, w, c, rho = NULL,
       }
     }
   } else if (lf == 2){
-    for (n in 1:nmax) {
+    for (n in ns) {
       for (i in 1:nrep) {
         loss <- numeric()
         sn <- numeric()
@@ -49,15 +50,15 @@ ssBNPearson2 <- function(lf, lam0, theta0, phi, w, c, rho = NULL,
       }
     }
   }
-  Y <- log(risk - c*rep(1:nmax, each = nrep))
-  mod <- lm(Y ~ I(log(rep(1:nmax + 1, each = nrep))))
+  Y <- log(risk - c*rep(ns, each = nrep))
+  mod <- lm(Y ~ I(log(rep(ns + 1, each = nrep))))
   E <- as.numeric(exp(mod$coef[1]))
   G <- as.numeric(-mod$coef[2])
   nmin <- ceiling((E*G/c)^(1/(G + 1))-1)
   if (plot == TRUE) {
-    plot(rep(1:nmax, each = nrep), risk, xlim = c(0, nmin + 0.1*nmax), xlab = "n")
+    plot(rep(ns, each = nrep), risk, xlim = c(0, nmax), xlab = "n", ylab = "CT(n)")
     curve <- function(x) {c*x + E/(1 + x)^G}
-    plot(function(x)curve(x), 0, nmin + 0.1*nmax, col = "blue", add = TRUE)
+    plot(function(x)curve(x), 0, nmax, col = "blue", add = TRUE)
     abline(v = nmin, col = "red")
   }
   cat("n =", nmin)
